5c406fd493e94f5838d133e1ff37f1289f839d07
Eric Schulte <schulte.eric@gmail.com>
2009-01-31 07:51:03 -0800

    last commit with separate repo and element methods, moving to simpler implementation

--- init.rb
+++ init.rb
@@ -1,3 +1,4 @@
 require 'active_file'
 require 'git'
+require 'acts_as_git'
 ActiveFile::Base.send(:include, ActiveFile::Acts::Git)
--- lib/acts_as_git.rb
+++ lib/acts_as_git.rb
@@ -2,9 +2,11 @@ module ActiveFile
   module Acts
     module Git
       require 'git'
-      
+
       def self.included(base)
         base.extend ActiveFile::Acts::Git::ClassMethods
+        # overwrite/wrap some methods
+        # this may be a good place to override methods
       end
 
       module ClassMethods
@@ -14,17 +16,20 @@ module ActiveFile
           return if self.included_modules.include?(ActiveFile::Acts::Git::RepoInstanceMethods)
           send(:include, ActiveFile::Acts::Git::RepoInstanceMethods)
         end
-        
+
         # ActiveFile objects which live inside of a git repository
         def acts_as_git
           return if self.included_modules.include?(ActiveFile::Acts::Git::InstanceMethods)
           send(:include, ActiveFile::Acts::Git::InstanceMethods)
         end
       end
-      
+
       module RepoInstanceMethods
+        @git
+        def git() @git end
+        def git=(git) @git = git end
       end
-      
+
       ## methods no Git::Base
       #
       # - cat file
@@ -35,29 +40,30 @@ module ActiveFile
       # - grep: returns an array of hashes and types
       # - remote:
       # - pull:
-      
+
       module InstanceMethods
-        
+
         # Return the information on the last commit at which this file
         # was changed.
         def last_commit
+
         end
         alias :last_update :last_commit
-        
+
         # Return the time at which this file was last updated
         def last_update_at
         end
-        
+
         # Return the name of the author of the last update
         #
         # (possibly return the name of the commit'r and the author)
         def last_update_by
         end
-        
+
         # Return the commit message of the last update
         def last_update_message
         end
-        
+
       end
     end
   end
