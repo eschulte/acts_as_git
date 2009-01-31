3ad61de0ff022e66dc9f0beaccad3451ff868d6f
Eric Schulte <schulte.eric@gmail.com>
2009-01-31 11:00:58 -0800

    basics are working

--- lib/acts_as_git.rb
+++ lib/acts_as_git.rb
@@ -40,28 +40,73 @@ module ActiveFile
             @git = Git.init(self.full_path)
           else
             # find the containing repo for an element
-            
+            # recurse up the directory tree until one is a git repo.
+            @git = nil
+            dir = File.dirname(self.full_path)
+            until @git
+              @git = Git.init(dir)
+              dir = File.join(dir, "..")
+            end
+            @git
           end
         end
+        
+        ## How to display a single file's history
+        # 
+        # http://www.gelato.unsw.edu.au/archives/git/0605/20453.html
+        # 
+        # probably with recent enough git, one of
+        #
+        # git whatchanged A
+        # 
+	# git log --stat -- A
+	# git log -p -- A
+	# git log -p --full-diff -- A
+        #
+        # actually *all* of the git commands can be limited by
+        # pathname
+        #
+        # with Ruby/Git you can limit a Git::Log object to a path, and
+        # then view only those commits which affected the path, so...
+        #
+        # 
+        
+        def commit_history
+          if self.class.directory?
+            commit_history_directory
+          else
+            commit_history_entry
+          end
+        end
+        def commit_history_entry
+          self.git.log.path(self.full_path)
+        end
+        def commit_history_directory
+          self.git.log.path(self.full_path)
+        end
+        alias :history :commit_history
         
         # Return the information on the last commit at which this file
         # was changed.
         def last_commit
-        end
-        alias :last_update :last_commit
-
-        # Return the time at which this file was last updated
-        def last_update_at
+          self.history.first
         end
 
-        # Return the name of the author of the last update
+        # Return the time at which this file was last commitd
+        def last_commit_at
+          self.last_commit.date
+        end
+
+        # Return the name of the author of the last commit
         #
         # (possibly return the name of the commit'r and the author)
-        def last_update_by
+        def last_commit_by
+          self.last_commit.author
         end
 
-        # Return the commit message of the last update
-        def last_update_message
+        # Return the commit message of the last commit
+        def last_commit_message
+          self.last_commit.message
         end
         
         # pass missing methods on through to Git
